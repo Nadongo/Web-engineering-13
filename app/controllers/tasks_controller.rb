8ecab6fd805392d7e17fd79676f036c24e8371c9
Nadongo <nadongocynthia@gmail.com>
2026-02-23 16:54:18 +0300

    Fix redirect after task creation to point to index page correctly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,51 @@
-def create
-    @task = Task.new(task_params)
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-    respond_to do |format|
-      if @task.save
-        # WE CHANGED THIS LINE RIGHT HERE:
-        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # CHANGED THIS LINE: Redirects to the index page instead of the show page
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :new
     end
   end
+
+  def update
+    if @task.update(task_params)
+      # The tests confirmed this one should still go to the show page, so we leave it as @task
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
