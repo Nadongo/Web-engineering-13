4bbf5e6e637dcf79b62659e57e579155a3a0e266
Nadongo <nadongocynthia@gmail.com>
2026-02-23 16:51:47 +0300

    Fix redirect after task creation to point to index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,14 @@
-class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def edit
-  end
-
-  def create
+def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to @task, notice: 'Task was successfully created.'
-    else
-      render :new
+
+    respond_to do |format|
+      if @task.save
+        # WE CHANGED THIS LINE RIGHT HERE:
+        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
-
-  def update
-    if @task.update(task_params)
-      redirect_to @task, notice: 'Task was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @task.destroy
-    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
-  end
-
-  private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:title, :content)
-  end
-end
