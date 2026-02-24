6cb5afe287f030c1a161000520170d88ace38e7e
Nadongo <nadongocynthia@gmail.com>
2026-02-24 12:16:46 +0300

    Change I18n and timezone to Japanese to pass the automated evaluation bot

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,11 @@ module CdpWebManyoTask
 
 
     #Time zone configuration
-    config.time_zone = 'Africa/Nairobi'
+    config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
 
-    #18n configuration
-    config.i18n.default_locale = :en
+    # Change default locale to Japanese
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,26 +5,21 @@ RSpec.describe 'Task management function', type: :system do
     context 'When registering a task' do
       it 'The registered task is displayed' do
         visit new_task_path
+        fill_in 'タイトル', with: 'My Test Task'
+        fill_in '内容', with: 'This is a test content.'
+        click_button '登録する'
         
-        # Updated to use the new i18n labels
-        fill_in 'Title', with: 'My Test Task'
-        fill_in 'content', with: 'This is a test content.'
-        click_button 'register'
-        
-        # Updated to expect the new flash message
-        expect(page).to have_content 'I have registered a task'
+        expect(page).to have_content 'タスクを登録しました'
         expect(page).to have_content 'My Test Task'
       end
     end
   end
 
   describe 'List display function' do
-    # Using let! to create 3 tasks with different dates before the tests run
     let!(:first_task) { FactoryBot.create(:task, title: 'first_task', created_at: '2025-02-18') }
     let!(:second_task) { FactoryBot.create(:task, title: 'second_task', created_at: '2025-02-17') }
     let!(:third_task) { FactoryBot.create(:task, title: 'third_task', created_at: '2025-02-16') }
 
-    # Using before to visit the page once before each 'it' block
     before do
       visit tasks_path
     end
@@ -37,10 +32,7 @@ RSpec.describe 'Task management function', type: :system do
       end
 
       it 'The list of created tasks is displayed in descending order of creation date and time.' do
-        # Grabs all the table rows inside the <tbody>
         task_list = all('tbody tr')
-        
-        # Checks if the newest task (first_task) is at the very top (index 0)
         expect(task_list[0]).to have_content 'first_task'
         expect(task_list[1]).to have_content 'second_task'
         expect(task_list[2]).to have_content 'third_task'
@@ -50,13 +42,11 @@ RSpec.describe 'Task management function', type: :system do
     context 'When creating a new task' do
       it 'New task is displayed at the top' do
         visit new_task_path
-        fill_in 'Title', with: 'newly_created_task'
-        fill_in 'content', with: 'new task content'
-        click_button 'register'
+        fill_in 'タイトル', with: 'newly_created_task'
+        fill_in '内容', with: 'new task content'
+        click_button '登録する'
 
-        expect(page).to have_content 'I have registered a task'
-        
-        # Verify the brand new task is at the very top of the list
+        expect(page).to have_content 'タスクを登録しました'
         task_list = all('tbody tr')
         expect(task_list[0]).to have_content 'newly_created_task'
       end
